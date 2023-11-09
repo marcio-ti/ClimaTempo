e7d612fa347fab89de78b0d56c56fabff048299a
Márcio dos Santos <marcio.economia@ulbra.edu.br>
2023-11-08 21:24:13 -0300

    Ajustes PEP8

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 """
 
 from datetime import datetime
+
 import dash_bootstrap_components as dbc
 import folium
 import plotly.express as px
@@ -24,26 +25,23 @@ from meteostat import Point, Daily
 """
 lat = -29.9153435
 lon = -51.2601959
-start = datetime( 2023, 1, 1 )
-end = datetime( 2023, 11, 6 )
-location = Point( lat, lon )
-data = Daily( location, start, end )
+start = datetime(2023, 1, 1)
+end = datetime(2023, 11, 6)
+location = Point(lat, lon)
+data = Daily(location, start, end)
 dados = data.fetch()
-
 
 """
     ? Manipulação dos dados do DataFrame
 """
-#? Cria a coluna Data atrabvés da cópia do index do DataFramme
+# ? Cria a coluna Data atrabvés da cópia do index do DataFramme
 dados['data'] = dados.index
 
-
-#? Seleciona apenas as colunas que serão utilizadas
+# ? Seleciona apenas as colunas que serão utilizadas
 dados = dados[['data', 'tavg', 'tmin', 'tmax', 'prcp', 'wspd', 'pres', 'tsun']]
 
-
-#? Renomeia as colunas
-dados.rename( columns={
+# ? Renomeia as colunas
+dados.rename(columns={
     'data': 'Data',
     'tavg': 'Temp. Média',
     'tmin': 'Temp. Min',
@@ -52,8 +50,7 @@ dados.rename( columns={
     'wspd': 'Veloc. Vento',
     'pres': 'Pressão',
     'tsun': 'Tempo de Sol'
-}, inplace=True )
-
+}, inplace=True)
 
 # --------------------------------------------------------------------------------------------------------
 """
@@ -61,23 +58,23 @@ dados.rename( columns={
 """
 
 #  Criando a Aplicação
-app = Dash( external_stylesheets=[dbc.themes.ZEPHYR] )
+app = Dash(external_stylesheets=[dbc.themes.ZEPHYR])
 
 # LAYOUT
 app.layout = html.Div(
     className='p-5',
     children=[
-        html.H4( 'Clima', className='mb-4' ),
+        html.H4('Clima', className='mb-4'),
         dcc.Dropdown(
             id='cidade_selecionada',
             options=['Porto Alegre'],
             value='',
             placeholder='Selecione uma cidade',
         ),
-        html.Div( id='dados', className='mt-4' ),
-        html.Div( id='mapa', className='mt-4' ),
+        html.Div(id='dados', className='mt-4'),
+        html.Div(id='mapa', className='mt-4'),
 
-        dbc.Label( "Variável escolhida" ),
+        dbc.Label("Variável escolhida"),
         dbc.RadioItems(
             options=[
                 {"label": "Temperatura", "value": 'Temperatura'},
@@ -87,20 +84,19 @@ app.layout = html.Div(
             value=1,
             id="grafico_variavel",
         ),
-        dcc.Graph( id='grafico' )
-    ] )
-
+        dcc.Graph(id='grafico')
+    ])
 
 
 # -------------------------------------------------------------------------------------------------------
 # Funções da Aplicação
 @callback(
-    Output( 'dados', 'children' ),
-    Output( "mapa", "children" ),
-    Input( 'cidade_selecionada', 'value' ),
+    Output('dados', 'children'),
+    Output("mapa", "children"),
+    Input('cidade_selecionada', 'value'),
     prevent_initial_call=True
 )
-def climaTempo(cidade):
+def clima_tempo(cidade):
     """
     Esta função gera um mapa e uma tabela de dados para a cidade selecionada.
 
@@ -117,24 +113,22 @@ def climaTempo(cidade):
     elif cidade == 'Porto Alegre':
 
         # Cria o mapa centralizando a cidade escolhida no centro
-        map = folium.Map(
+        mapa = folium.Map(
             location=[lat, lon], zoom_start=10, tiles="cartodb positron"
         )
         # Salva o Mapa no formato html
-        map.save( "mapa_cidade.html" )
-
-
+        mapa.save("mapa_cidade.html")
 
         # Faz uma cópia do DataFrame para manipulação dos dados
         dados_tabela = dados.copy()
         # Ordena a tabela com base nos valores do tipo Data de maneira decrescente
-        dados_tabela = dados_tabela.sort_values( by=['Data'], ascending=False )
+        dados_tabela = dados_tabela.sort_values(by=['Data'], ascending=False)
         # Retira os valores referentes ao tempo, transformando a Data em uma String
-        dados_tabela['Data'] = dados_tabela['Data'].dt.strftime( '%d-%m-%Y' )
+        dados_tabela['Data'] = dados_tabela['Data'].dt.strftime('%d-%m-%Y')
 
         # Retorno da Função
         return (dash_table.DataTable(
-            dados_tabela.to_dict( "records" ),
+            dados_tabela.to_dict("records"),
             style_cell={"textAlign": "center"},
             page_size=30,
             style_data_conditional=[
@@ -146,7 +140,7 @@ def climaTempo(cidade):
 
         ),
                 html.Iframe(
-                    srcDoc=open( "mapa_cidade.html", "r" ).read(),
+                    srcDoc=open("mapa_cidade.html", "r").read(),
                     style={"width": "100%", "height": "600px"},
                     className="d-flex aligns-items-center justify-content-center rounded mx-auto p-4 ",
                 )
@@ -154,8 +148,8 @@ def climaTempo(cidade):
 
 
 @callback(
-    Output( 'grafico', 'figure' ),
-    Input( 'grafico_variavel', 'value' ),
+    Output('grafico', 'figure'),
+    Input('grafico_variavel', 'value'),
     prevent_initial_call=True
 )
 def grafico(tipo_grafico):
@@ -181,22 +175,22 @@ def grafico(tipo_grafico):
                     go.Scatter(
                         x=dados['Data'],
                         y=dados['Temp. Média'],
-                        line=dict( color="blue", width=2 ),
+                        line=dict(color="blue", width=2),
                         name="Temperatura Média",
                     ),
                     go.Scatter(
                         x=dados['Data'],
                         y=dados['Temp. Max'],
-                        line=dict( color="red", width=2 ),
+                        line=dict(color="red", width=2),
                         name="Temperatura Máxima",
                     ),
                     go.Scatter(
                         x=dados['Data'],
                         y=dados['Temp. Min'],
-                        line=dict( color="green", width=2 ),
+                        line=dict(color="green", width=2),
                         name="Temperatura Mínima",
                     )
-                ] )
+                ])
             return fig
         elif tipo_grafico == 'Vento':
             fig = go.Figure(
@@ -204,19 +198,18 @@ def grafico(tipo_grafico):
                     go.Scatter(
                         x=dados['Data'],
                         y=dados['Veloc. Vento'],
-                        line=dict( color="blue", width=2 ),
+                        line=dict(color="blue", width=2),
                         name="Velocidade do Vento",
                     )
-                ] )
+                ])
             return fig
         elif tipo_grafico == 'Precipitacao':
-            fig = px.bar( dados, x=dados['Data'], y=dados['Precipitação'] )
+            fig = px.bar(dados, x=dados['Data'], y=dados['Precipitação'])
             return fig
-
 
 
 # ----------------------------------------------------------------------------------------------------------
 # SERVIDOR
 if __name__ == '__main__':
     # app.run( debug=True )
-    app.run( host="0.0.0.0", port="8080" )
+    app.run(host="0.0.0.0", port="8080")
